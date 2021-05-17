27a3f3abd2830c7341c35be29e3af7a98cdd7c40
CrossOv3r <allanhivdc@gmail.com>
2021-05-16 23:58:53 -0300

    Iniciando Header

---
+++ header.php
@@ -0,0 +1,26 @@
+<header>
+    <div class="header">
+        <div class="account">
+            <div class="img">
+                <div class="imgbc">
+                    <div class="foto">
+                        <img src="./public/img/user.png" alt="">
+                    </div>
+                </div>
+               <div class="nick">
+                    <span>HHHHHHHHHHH</span>
+               </div>
+            </div>
+            <div class="right">
+                <div class="life">
+                    <i class="fa fa-heart" aria-hidden="true"></i>
+                    <i class="fa fa-heart" aria-hidden="true"></i>
+                    <i class="fa fa-heart-o" aria-hidden="true"></i>
+                </div>
+                <div class="gear">
+                    <span class="iconify" data-icon="raphael:smallgear" data-inline="false"></span>
+                </div>
+            </div>
+        </div>
+    </div>   
+</header>
